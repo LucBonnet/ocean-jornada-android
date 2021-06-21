f7a76b291fda2587b2f3f2000dbcd79596b81b13
Luc <luc@Jelubo.local>
2021-06-21 12:57:45 -0300

    funcao do botao

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao/MainActivity.kt
@@ -2,10 +2,24 @@ package com.oceanbrasil.ocean_android_introducao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* val -> imutavel */
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val btEnter = findViewById<Button>(R.id.btEnter)
+        val etName = findViewById<EditText>(R.id.etName)
+
+        btEnter.setOnClickListener {
+            tvResult.text = etName.text.toString()
+        }
+
+
     }
 }
