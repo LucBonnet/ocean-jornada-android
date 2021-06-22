17b1affc17db5b6321268d848e03a53efea26ffe
Luc <luc@Jelubo.local>
2021-06-22 12:33:51 -0300

    Botao voltar

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao/SecondActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao/SecondActivity.kt
@@ -1,11 +1,20 @@
 package com.oceanbrasil.ocean_android_introducao
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val btBack = findViewById<Button>(R.id.btBack)
+
+        btBack.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+        }
     }
 }
